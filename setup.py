6b021b44b3f07e0c931f7df4130f3e925d1cd265
Robert Fennis <Robert.Fennis@ext.esa.int>
2025-03-06 15:43:04 +0100

    first version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycasso",
+    version="0.1.0",
+    description="A description of your library",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],  # Add dependencies here
+)
